e68f3aa56e951944676d8bf5c2b2dd07b0d6a48f
wyunchi-ms <yunwang@microsoft.com>
2022-10-28 17:08:40 +0800

    UPdate

--- src/Accounts/Accounts/Account/ConnectAzureRmAccount.cs
+++ src/Accounts/Accounts/Account/ConnectAzureRmAccount.cs
@@ -51,8 +51,7 @@ namespace Microsoft.Azure.Commands.Profile
     /// <summary>
     /// Cmdlet to log into an environment and download the subscriptions
     /// </summary>
-    [Cmdlet("Connect", AzureRMConstan
-    ts.AzureRMPrefix + "Account", DefaultParameterSetName = UserParameterSet, SupportsShouldProcess=true)]
+    [Cmdlet("Connect", AzureRMConstants.AzureRMPrefix + "Account", DefaultParameterSetName = UserParameterSet, SupportsShouldProcess=true)]
     [Alias("Login-AzAccount", "Login-AzureRmAccount", "Add-" + AzureRMConstants.AzureRMPrefix + "Account")]
     [OutputType(typeof(PSAzureProfile))]
     public class ConnectAzureRmAccountCommand : AzureContextModificationCmdlet, IModuleAssemblyInitializer
